5e817dd0a461690a33452b37f06cd9497d85a687
Kellyquintesa <kellyquintesa11@gmail.com>
2022-02-23 21:12:41 +0700

    gitpush

--- food-app/pages/_app.js
+++ food-app/pages/_app.js
@@ -1,29 +1,23 @@
+import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
 
+
+
 const colors = {
-  brand: {
-    Orange: '#FA5A1E',
-   
+  foodiez: {
+    orange: '#FA5A1E'
   },
 }
 
 const theme = extendTheme({ colors })
 
-// 3. Pass the `theme` prop to the `ChakraProvider`
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  )
-}
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+   
   )
 }
 
--- food-app/pages/index.js
+++ food-app/pages/index.js
@@ -1,25 +1,65 @@
-import Link from 'next/link'
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { Box, Flex, Spacer, Text, Button, ButtonGroup } from "@chakra-ui/react";
+import Image from "next/image";
 
- function Home() {
+const Home = () => {
+  const images = [
+    <div className="1">
+      <img src="/images/login-1.png" />
+    </div>,
+    <div className="2">
+      <img src="/images/login-2.png" />
+    </div>,
+    <div className="3">
+      <img src="/images/login-3.png" />
+    </div>,
+  ];
+
+  const renderCarousel = () => {
+    <AliceCarousel
+      autoPlay
+      autoPlayInterval="3000"
+      items={images}
+      animationType="fadeout"
+      animationDuration={800}
+      disableButtonsControls
+      infinite
+      mouseTracking
+    />;
+  };
+
   return (
-    <ul>
-    <li>
-      <Link href="/loginForm/login">
-        <a>login1</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/loginForm/login2">
-        <a>About Us</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/loginForm/login3">
-        <a>Blog Post</a>
-      </Link>
-    </li>
-  </ul>
-  )
-}
+    <Box>
+      <Flex textAlign="center" padding={10}>
+        <Box w="70px" h="10" />
+        <Spacer />
+        <Box w="170px" h="10">
+          <Image src="/images/icon.png" width={35} height={24} />
+        </Box>
+        <Spacer />
+        <Box w="70px" h="10" fontWeight={700} color="foodiez.orange">
+          Skip
+        </Box>
+      </Flex>
+      {renderCarousel()}
+      <Box padding={5}>
+        <Button
+          borderRadius={20}
+          width="full"
+          marginBottom={5}
+          color="white"
+          backgroundColor="foodiez.orange"
+        >
+          Create an account
+        </Button>{" "}
+        <br />
+        <Button borderRadius={20} width="full" color="foodiez.orange">
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
